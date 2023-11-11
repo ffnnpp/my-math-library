2b546af0f6fea9730bd1fcdbd81d396e3784e733
fern <napasorn18190@gmail.com>
2023-11-11 11:39:38 +0700

    Use Array.reduce

--- math.js
+++ math.js
@@ -1,11 +1,10 @@
 function sum(a, b) {
     if (Array.isArray(a)) {
-      let total = 0
-      a.forEach((i) => (total += i))
-      return total
-    } else if (typeof a === 'number' && typeof b === 'number') {
-      return a + b
-    }
+        return a.reduce((acc, n) => acc + n, 0)
+        //comment
+      } else if (typeof a === 'number' && typeof b === 'number') {
+        return a + b
+      }
   }
 module.exports = {
     sum,
