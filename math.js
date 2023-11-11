a1679abc1b669f1e2edaaf48090fb26327e4a40c
fern <napasorn18190@gmail.com>
2023-11-11 11:35:43 +0700

    Use Array.forEach

--- math.js
+++ math.js
@@ -1,9 +1,7 @@
 function sum(a, b) {
     if (Array.isArray(a)) {
       let total = 0
-      for (let i = 0; i < a.length; i++) {
-        total += a[i]
-      }
+      a.forEach((i) => (total += i))
       return total
     } else if (typeof a === 'number' && typeof b === 'number') {
       return a + b
